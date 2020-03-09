ba3f403cf3c76f1314cd5d3e801aa8f73f82a75d
claire froelich <claire.froelich@gmail.com>
2020-03-09 19:46:04 -0400

    add cors directly to route

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors({
-  credentials: true,
-  origin: 'https://localhost:8000'
-}));
+// app.use(cors({
+//   credentials: true,
+//   origin: 'https://localhost:8000'
+// }));
 
 const port = process.env.PORT || 3000
 
@@ -19,7 +19,7 @@ app.get('/', (req, res) => res.send('Go away!'))
 
 app.get('/contacts', (req, res) => res.send('contact endpoint'))
 
-app.post('/contacts', (req, res) => {
+app.post('/contacts', cors(), (req, res) => {
   console.log(req.body);
   return res.send(`received ${req.body.email}`);
 });
