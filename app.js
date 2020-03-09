17ec28aa3825d312597de5b24bd801872673eb89
claire froelich <claire.froelich@gmail.com>
2020-03-09 19:58:03 -0400

    attempt to pass preflight

--- app.js
+++ app.js
@@ -4,10 +4,27 @@ const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const whitelist = ['http://localhost:8000/', 'https://localhost:8000/']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 const app = express();
 app.use(bodyParser.json());
 //allow OPTIONS on all resources
-app.options('*', cors())
+// app.options('*', cors())
+//
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 const port = process.env.PORT || 3000
 
@@ -17,7 +34,7 @@ app.get('/', (req, res) => res.send('Go away!'))
 
 app.get('/contacts', (req, res) => res.send('contact endpoint'))
 
-app.post('/contacts', (req, res) => {
+app.post('/contacts', cors(corsOptions),(req, res) => {
   console.log(req.body);
   return res.send(`received ${req.body.email}`);
 });
