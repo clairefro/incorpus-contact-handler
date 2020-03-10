2ff14c150775eb15cd5c28b449c5b04d79890309
claire froelich <claire.froelich@gmail.com>
2020-03-10 11:14:56 -0400

    new options

--- app.js
+++ app.js
@@ -5,20 +5,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 var whitelist = ['http://localhost:8000', 'https://localhost:8000']
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
+// var corsOptionsDelegate = function (req, callback) {
+//   var corsOptions;
+//   if (whitelist.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false } // disable CORS for this request
+//   }
+//   callback(null, corsOptions) // callback expects two parameters: error and options
+// }
 
 const app = express();
 app.use(bodyParser.json());
 //allow OPTIONS on all resources
-app.options('*', cors())
+app.options('*', cors({credentials: true, origin: true}))
 
 const port = process.env.PORT || 3000
 
@@ -28,7 +28,7 @@ app.get('/', (req, res) => res.send('Go away!'))
 
 app.get('/contacts', (req, res) => res.send('contact endpoint'))
 
-app.post('/contacts', cors(corsOptionsDelegate),(req, res) => {
+app.post('/contacts', (req, res) => {
   console.log(req.body);
   return res.send(`received ${req.body.email}`);
 });
